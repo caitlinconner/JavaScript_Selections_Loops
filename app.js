dcd18980b0a46121721549033864aa66b18750e6
Caitlin Conner <caitlinjconner@gmail.com>
2023-04-27 19:24:45 -0500

    Fixed Comment Issues

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Hello World!\n==========\n");
 
-Exercise 1 Section
+// Exercise 1 Section
 
 
 for (let i = 0; i < 100; i++) {
@@ -13,33 +13,33 @@ for (let i = 0; i < 100; i++) {
 
 //while below
 
-let i = 1; 
+// let i = 1; 
 
-while (i < 100) {
-    if (i % 2 == 1) {
-        console.log(i)
-    }
-    i++;
-}
+// while (i < 100) {
+//     if (i % 2 == 1) {
+//         console.log(i)
+//     }
+//     i++;
+// }
 
 // do while below
 
 
-let x = 1;
+// let x = 1;
 
-do {
-    if (x % 2 == 1) {
-      console.log(x);
-}
-    x++;
-} while (x < 100);
+// do {
+//     if (x % 2 == 1) {
+//       console.log(x);
+// }
+//     x++;
+// } while (x < 100);
     
 
 
-console.log("EXERCISE 1:\n==========\n");
+// console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+// console.log("EXERCISE 2:\n==========\n");
 
 for (let i = 1; i < 100; i++) {
     //only use two conditionals & one console.log
@@ -56,19 +56,19 @@ for (let i = 1; i < 100; i++) {
     console.log(output);
 }
 
-for (let i = 1; i < 100; i++) {
-    console.log(i);
+// for (let i = 1; i < 100; i++) {
+//     console.log(i);
 
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log(i, 'FIZZBUZZ');
-    } else if (i % 5 == 0) {
-        console.log(i, 'BUZZ');
-    } else if (i % 3 == 0) {
-        console.log(i, 'FIZZ');
-    }
-}
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log(i, 'FIZZBUZZ');
+//     } else if (i % 5 == 0) {
+//         console.log(i, 'BUZZ');
+//     } else if (i % 3 == 0) {
+//         console.log(i, 'FIZZ');
+//     }
+// }
 
-while below
+// while below
 
 let i = 1;
 
@@ -84,20 +84,20 @@ while (i < 100) {
     i++;
 }
 
-let x = 1;
+// let x = 1;
 
-do {
-    if (x % 3 == 0 && x % 5 == 0) {
-        console.log(x, 'FIZZBUZZ');
-    } else if (x % 5 == 0) {
-        console.log(x, 'BUZZ');
-    } else if (x % 3 == 0) {
-        console.log(x, 'FIZZ');
-    }
-    x++;
-} while (x < 100);
+// do {
+//     if (x % 3 == 0 && x % 5 == 0) {
+//         console.log(x, 'FIZZBUZZ');
+//     } else if (x % 5 == 0) {
+//         console.log(x, 'BUZZ');
+//     } else if (x % 3 == 0) {
+//         console.log(x, 'FIZZ');
+//     }
+//     x++;
+// } while (x < 100);
 
-exercise 4
+// exercise 4
 
 let value = Math.round(Math.random() * 500); // creates a random number between 0 and 500
 let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
@@ -112,38 +112,38 @@ for (let i = 1; i <= n; i++) {
     }
 }
 
-using boolean
+// using boolean
 
-let foundValue = false;
+// let foundValue = false;
 
-for (let i = 1; i <= n; i++) {
-    if (i == value) {
-        foundValue = true;
-        break;
-    }
-}
+// for (let i = 1; i <= n; i++) {
+//     if (i == value) {
+//         foundValue = true;
+//         break;
+//     }
+// }
 
-if (foundValue) {
-        console.log(`Found ${value}!`);
-} else {
-        console.log(`Did not find ${value} within 1-${n}`);
-}
+// if (foundValue) {
+//         console.log(`Found ${value}!`);
+// } else {
+//         console.log(`Did not find ${value} within 1-${n}`);
+// }
 
-let foundValue;
+// let foundValue;
 
-for (let i = 1; 1 <= n && !foundValue; i++) {
-    foundValue = (i == value);
-}
+// for (let i = 1; 1 <= n && !foundValue; i++) {
+//     foundValue = (i == value);
+// }
 
- if (foundValue) {
-            console.log(`Found ${value}!`);
-    } else {
-            console.log(`Did not find ${value} within 1-${n}`);
-    }
+//  if (foundValue) {
+//             console.log(`Found ${value}!`);
+//     } else {
+//             console.log(`Did not find ${value} within 1-${n}`);
+//     }
 
 
 
-exercise 5
+// exercise 5
 
 let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
 let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
